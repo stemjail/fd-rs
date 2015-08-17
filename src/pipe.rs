8b827b914a591c9890871620bfd224e9f9396754
Mickaël Salaün <mic@digikod.net>
2015-08-17 17:42:05 +0200

    pipe: Create FDs with O_CLOEXEC

--- src/pipe.rs
+++ src/pipe.rs
@@ -12,13 +12,28 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
-use libc::c_int;
 use std::fs::File;
 use std::io;
 use std::os::unix::io::FromRawFd;
 
-/// A `pipe(2)` interface.
+mod raw {
+    use libc::c_int;
+
+    // From asm-generic/fcntl.h
+    pub const O_CLOEXEC: c_int = 0o2000000;
+
+    #[repr(C)]
+    pub struct PipeFds {
+        pub reader: c_int,
+        pub writer: c_int,
+    }
+
+    extern {
+        pub fn pipe2(fds: *mut PipeFds, flags: c_int) -> c_int;
+    }
+}
+
+/// A thread-safe `pipe(2)` interface.
 ///
 /// Create a reader and a writer `File` for each part of the pipe.
 pub struct Pipe {
@@ -28,15 +43,16 @@ pub struct Pipe {
 
 impl Pipe {
     pub fn new() -> io::Result<Pipe> {
-        let mut fds: (c_int, c_int) = (-1, -1);
-        let fdp: *mut c_int = unsafe { ::std::mem::transmute(&mut fds) };
-        // TODO: Use pipe2(2) with O_CLOEXEC
-        if unsafe { ::libc::pipe(fdp) } != 0 {
+        let mut fds = raw::PipeFds {
+            reader: -1,
+            writer: -1
+        };
+        if unsafe { raw::pipe2(&mut fds, raw::O_CLOEXEC) } != 0 {
             return Err(io::Error::last_os_error());
         }
         Ok(Pipe {
-            reader: unsafe { File::from_raw_fd(fds.0) },
-            writer: unsafe { File::from_raw_fd(fds.1) },
+            reader: unsafe { File::from_raw_fd(fds.reader) },
+            writer: unsafe { File::from_raw_fd(fds.writer) },
         })
     }
 }
